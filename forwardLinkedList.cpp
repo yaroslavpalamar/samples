a46576d4e8e6843d3c31adac46b91ea1f9cdfade
Yaroslav Palamar <yaroslav.palamar@gmail.com>
2018-05-04 17:50:15 +0300

    added methods to List class: removeAt, insert, push_front, pop_back

--- forwardLinkedList.cpp
+++ forwardLinkedList.cpp
@@ -20,6 +20,11 @@ class List {
         void pop_front();
         void clear();
     
+        void push_front(T data);
+        void insert(int index, T val);
+        void removeAt(int index);
+        void pop_back();
+    
     private:
         template <typename B>
         class Node {
@@ -93,6 +98,52 @@ void List<T>::clear() {
     }
 }
 
+template<typename T>
+void List<T>::push_front(T data) {
+    // point your attantion to second parameter head
+    head = new Node<T>(data, head);
+    size++;
+}
+
+template<typename T>
+void List<T>::insert(int index, T val) {
+    Node<T> * previous = this->head;
+    
+    if (index == 0) {
+        push_front(val);
+    } else {
+        for (int i = 0; i< index; i++) {
+            previous = head->pNext;
+        }
+    }
+    Node<T> *newNode = new Node<T>(val, previous->pNext);
+    previous->pNext=newNode;
+    size++;
+}
+
+template<typename T>
+void List<T>::removeAt(int index) {
+    Node<T> * previous = this->head;
+    
+    if (index == 0) {
+        pop_front();
+    } else {
+        for (int i = 0; i< index; i++) {
+            previous = head->pNext;
+        }
+    }
+    Node<T> *nodeToDelete = previous->pNext;
+    previous->pNext = nodeToDelete->pNext;
+    delete nodeToDelete;
+    size--;
+}
+
+
+template<typename T>
+void List<T>::pop_back(){
+    removeAt(size-1);
+}
+
 int main() {
     List<int> lst;
     lst.push_back(1);
@@ -109,10 +160,10 @@ int main() {
     lst.pop_front();
     
     // check the size of linked list after removing
-    cout << "size of linked list after pop_front " << lst.getSize() << endl;
+    cout << "size of linked list after pop_front " << lst.getSize() << endl  << endl;
     
     lst.clear();
-    cout << "size of linked list after method clear() " << lst.getSize() << endl;
+    cout << "size of linked list after method clear() " << lst.getSize() << endl << endl;
     
     // push_back randomly 10 numbers inside list (from 0 to 10)
     int numberCount = 10 ;
@@ -122,8 +173,31 @@ int main() {
     }
     
     // display all data from linked list:
-    cout << "print all data in linked list: " << endl;
+    cout << "print all data in linked list: " << endl << endl;
     for (int i; i < lst.getSize(); i++) {
         cout << lst[i] << endl;
     }
+    
+    // push_front() method test
+    lst.push_front(11);
+    lst.push_front(12);
+    cout << "display fist two nodes: first: " << lst[0] << " second: " << lst[1] <<  endl << endl;
+    
+    // insert method check
+    cout << "insert two values by index 3 and 2 with values 3000 and 4444" << endl;
+    lst.insert(3, 3000);
+    lst.insert(2, 4444);
+    cout << "display two nodes by index: index 3: " << lst[3] << " index 2: " << lst[2] <<  endl << endl;
+    
+    // removeAt() function test
+    cout << "call removeAt(2) fuction" << endl;
+    lst.removeAt(2);
+    cout << "compare with previous result node 2, check whether node was changed after removeAT(): " << lst[2]  <<  endl << endl;
+    
+    // check pop_back method 
+    cout << "size of linked list before pop_back " << lst.getSize() << endl;
+    cout << "print current last element: " << lst[lst.getSize()-1]  <<  endl;
+    lst.pop_back();
+    cout << "size of linked list after pop_back " << lst.getSize() << endl;
+    cout << "print last element and compare with previous one: " << lst[lst.getSize()-1]  <<  endl;
 }
