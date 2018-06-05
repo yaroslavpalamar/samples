b5982c7d1de2b79792b1f26340cccf3e48470ef3
Yaroslav Palamar <yaroslav.palamar@gmail.com>
2018-06-05 13:00:17 +0300

    added sample with function pointer

---
+++ functionPointer.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*Benefits from usage function pointer:
+You can call different functions with similar declaration but without flags.
+For example you can have one logic in function showInfo() but showInfo() will call
+any DataFromBD() or DataFromWebServer() depends on the parameters.
+*/
+
+string getDataFromBD()
+{
+	return "Data From BD";
+}
+
+string getDataFromWebServer()
+{
+	return "Data From WebServer";
+}
+
+string getDataFromServer2()
+{
+	return "Data From Server2";
+}
+
+void showInfo (string (*printData)()) 
+{
+	cout << (*printData)() << endl;
+}
+
+int main() {
+	showInfo(getDataFromWebServer);
+	showInfo(getDataFromServer2);
+	showInfo(getDataFromBD);
+	return 0;
+}
+
+
+
