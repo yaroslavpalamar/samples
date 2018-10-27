54bbde91343256d75d41684fa2d1b18d56bd7f6a
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2018-10-27 22:04:34 +0300

    finished implementation for hash table

--- hashingWithChaining.C
+++ hashingWithChaining.C
@@ -11,7 +11,7 @@ using namespace std;
 
 
 class HashChain {
-	int container;
+	int hashTablSize;
 	
 	// array with indexes to the lists(chains)
 	list<int> *table;
@@ -20,15 +20,16 @@ public:
 	void insertItem(int x);
 	void delItem(int key);
 	int hashFunct(int x) {
-		return (x % container);
+		return (x % hashTablSize);
 	}
 	void printHash();
+	bool isItemInTable (int val);
 };
 
 HashChain::HashChain(int data)
 {
-	this->container = data;
-	table = new list<int>[container];
+	this->hashTablSize = data;
+	table = new list<int>[hashTablSize];
 }
 
 void HashChain::insertItem(int key)
@@ -54,21 +55,59 @@ void HashChain::delItem(int key)
 		table[index].erase(i);
 }
 
+bool HashChain::isItemInTable(int val)
+{
+	int index = hashFunct(val);
+	for (list<int>::iterator i = table[index].begin(); i !=table[index].end(); ++i) {
+		if (*i==val) {
+			return true;
+		}
+	}
+	return false;
+}
+
 void HashChain::printHash()
 {
-	for (int i = 0; i < container; ++i) {
+	for (int i = 0; i < hashTablSize; ++i) {
 		cout << i;
 		for (auto x : table [i]) {
 			cout << " -> " <<x;
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
 
 int
 main (int argc, char** argv)
 {
+	// array with keys
+	int arr[] = {10, 14, 44, 99, 12, 19,20,3,4};
 
+	// insert the keys to hash table
+	HashChain h(7);
+
+	for (int i : arr) {
+		h.insertItem(i);
+	}
+	
+	cout << "Print added data to the table:"<<endl;	
+	h.printHash();
+	
+	h.delItem(4);
+	h.delItem(10);
+	h.delItem(10);
+	
+
+	// print after deletion
+	cout << "Print table after deletion:" << endl;
+	h.printHash();
+	
+	cout << "Print 1 if item exist in the table and 0 if not:" << endl;
+	cout << "Item 99: " << h.isItemInTable(99) << endl;
+	cout << "Item 4: " << h.isItemInTable(4) << endl;
+	cout << "Item 12: " << h.isItemInTable(12) << endl;
+	cout << "Item 12: " << h.isItemInTable(19) << endl;
 
 	return 0;
 }
