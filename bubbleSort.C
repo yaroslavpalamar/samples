67aedd9e957ce95fbc7a57feaa80e7c17fae6ee8
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2018-11-01 17:31:39 +0200

    added bubble sort implementation

---
+++ bubbleSort.C
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void swap (int *a, int *b)
+{
+	int tmp = *a;
+	*a=*b;
+	*b=tmp;
+}
+
+void bubbleSort(int* arr, int arrSize)
+{
+	for (int i=0; i< arrSize-1; ++i) {
+		for (int j=0; j<arrSize-i-1; ++j) {
+			if (arr[j] > arr[j+1])
+				swap(arr[j], arr[j+1]);
+		}
+	}
+}
+
+void printArr(int* arr, int arrSize) 
+{
+	for (int i=0; i<arrSize; ++i) {
+		cout << arr[i] << endl;
+	}
+}
+
+int
+main (int argc, char** argv)
+{	
+	int arr[] = {23,31,12,1,2,100,900,7,4,3,2,0,-1,345,45,4,2,4,6,8,9,0,0};
+	int arrSize = sizeof(arr)/sizeof(arr[0]);
+	
+
+	cout << "Arr size is: " << arrSize << endl;
+	bubbleSort(arr, arrSize);
+	printArr(arr, arrSize);
+
+	return 0;
+}
+
