4b7d42b7991edc6130d798805c8c6513fa1cef37
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2018-11-01 12:14:47 +0200

    Added comments to insertion sort and fixed bug, initialized index to 1

--- insertionSort.C
+++ insertionSort.C
@@ -3,17 +3,20 @@ using namespace std;
 
 void insertionSort(int* arr, int arrSize)
 {
-	int key, j;
-	for (int i; i< arrSize; ++i) {
-		key=arr[i];
-		j = i - 1;
+	int keyVal, iPrev;
+	for (int i=1; i< arrSize; ++i) {
+		// save element for compare
+		keyVal=arr[i];
+		iPrev = i - 1;
 
-		while (j >= 0 && arr[j] > key)
+		// move back to the beginning array and compare key Value with prevous
+		// if less tnan previous just swap the values
+		while (iPrev >= 0 && arr[iPrev] > keyVal)
 		{
-			arr[j+1] = arr[j];
-			j = j-1;
+			arr[iPrev+1] = arr[iPrev];
+			iPrev = iPrev-1;
 		}
-		arr[j+1]=key;
+		arr[iPrev+1]=keyVal;
 	}
 }
 
@@ -27,9 +30,11 @@ void printArr(int* arr, int arrSize)
 int
 main (int argc, char** argv)
 {	
-	int arr[] = {23,31,12,1,2,100,900};
+	int arr[] = {23,31,12,1,2,100,900,7,4,3,2,0,-1,345,45,4,2,4,6,8,9,0,0};
 	int arrSize = sizeof(arr)/sizeof(arr[0]);
 	
+
+	cout << "Arr size is: " << arrSize << endl;
 	insertionSort(arr, arrSize);
 	printArr(arr, arrSize);
 
