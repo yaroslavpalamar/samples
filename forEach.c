05913ead15c27f8c9d9fd13c0e86a09a948d27a4
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2018-11-05 21:33:45 +0200

    added for_echa implementation in C

--- forEach.c
+++ forEach.c
@@ -18,18 +18,18 @@ main(int argc, char**argv)
 	};
 
 	struct list_node listInt[] = {
-                {.next= &listInt[1], .data="100"},
-                {.next= &listInt[2], .data="200"},
-                {.next= NULL, .data="3000"}
+                {.next= &listInt[1], .data=(int*)100},
+                {.next= &listInt[2], .data=(int*)200},
+                {.next= NULL, .data=(int*)300}
         };
 
 	FOR_EACH(item, listChar) {
 		printf("%s ", item->data );
 	}
-		printf("\n");
+	printf("\n");
 	FOR_EACH(item, listInt) {
-                printf("%s ", item->data );
-                printf("\n");
+                printf("%d ", (int)item->data );
 	}
+	printf("\n");
 }
 
