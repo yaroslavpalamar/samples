97014eb08b568a853489ebec41d52c5903ea644a
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2018-11-29 17:19:32 +0200

    added implementation of stack using priority queue

---
+++ data_structures/stackWithPQorHeap.cpp
@@ -0,0 +1,58 @@
+#include <queue>
+#include <iostream>
+using namespace std;
+
+typedef pair<int, int> p;
+
+class Stack {
+	int cnt;
+	priority_queue<pair<int,int>> pq;
+public:
+	Stack()
+	{
+		cnt = 0;
+	}
+	void push(int val) 
+	{
+		cnt++;
+		pq.push(pair<int, int>(cnt,val));
+	}
+	void pop() 
+	{
+		if (pq.empty()) {
+			cout << "Nothing to pop";
+		}
+		cnt--;
+		pq.pop();
+	}
+
+	int top() 
+	{
+		p tmp=pq.top();
+		return tmp.second;
+	}
+	bool isEmpty()
+	{
+		return pq.empty();
+	}
+};
+
+int 
+main (int argc, char** argv)
+{
+	cout << "push 1,2,3 to stack" << endl;
+	Stack s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	
+	cout << "pop from stack:" << endl;
+	while(!s.isEmpty()) {
+		cout << s.top()<<endl;
+		s.pop();
+	}
+
+	return 0;
+}
+
+
