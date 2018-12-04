bb01ecac89bb21571e87843b99c95fef81d687c8
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2018-12-04 18:07:11 +0200

    added better solution in task for removing duplicates from array

--- interview_tasks/c_implementation/removeDuplFromArr.c
+++ interview_tasks/c_implementation/removeDuplFromArr.c
@@ -23,20 +23,54 @@ void removeDuplFromArray(char* arr, int arrLength) {
 	}
 	(*++result) = '\0';
 }
+
+int foundInArr(char val, char* arr, int arrLength) 
+{
+	for (int i = 0; i < arrLength; ++i) {
+		if (val == arr[i])
+			return 1;
+	}
+	return 0;
+}
+
+void removeDuplFromArray2(char* arr, int arrLength) {
+	char checkArr[255]={0};
+	char *first = arr;
+	char* last = arr+arrLength;
+	char* result = first;
+
+	int i=0;
+	checkArr[i++] = *result;
+	while (++first != last) {
+		if (!foundInArr(*first, checkArr, strlen(checkArr))) {
+			checkArr[i++]=*(first);
+			*(++result)=*first;
+		}
+	}
+	(*++result) = '\0';
+}
  
 int main(void) 
 {
-	char arr[]={4,6,5,4,3,3, 7,7,7,5,5,5 ,5,8,2,10};
+	printf ("Original array:\n");
+	char arr[]={4,6,5,4,3,3,7,7,7,5,5,5,5,8,2,10};
 	int arrLength = sizeof(arr);
 
 	for(int n=0; n < arrLength; n++)
 	printf("%d ",arr[n]);
 	printf("\n");
 
+	printf ("Call first function to remove dupl from arr:\n");
 	removeDuplFromArray(arr, arrLength);
-	
 	for(int n=0; n < strlen(arr); n++)
-	printf("%d ",arr[n]);
+		printf("%d ",arr[n]);
+
+	
+	char arr2[]={4,6,5,4,3,3, 7,7,7,5,5,5 ,5,8,2,10};
+	printf ("\nCall second function to remove dupl from arr:\n");
+	removeDuplFromArray2(arr2, arrLength);
+	for(int n=0; n < strlen(arr2); n++)
+		printf("%d ",arr2[n]);
 	return 0;
 }
 
