799910d2bae8b3c3e842801d65872334f6726a2a
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2018-12-05 19:24:21 +0200

    added memmove and memcpy implementation

---
+++ interview_tasks/c_implementation/memMoveMemCpy.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void memCpy(void* dest, void *src, size_t n)
+{
+	char *cSrc = (char *) src;
+	char *cDest = (char *) dest;
+
+	// Copy contents of src[] to dest[]
+	for (int i = 0; i < n; i++)
+		cDest[i] = cSrc[i];
+}
+
+//memmove used just when data overlaps and corrupt buffer 
+//so for memmove need to use tmp buffer
+void memMove(void* dest, void *src, size_t n)
+{
+	char *cSrc = (char *) src;
+	char *cDest = (char *) dest;
+
+	// Create temp arr
+	char *tmp = (char*) malloc(n);
+	for (int i = 0; i < n; i++)
+		tmp[i] = cSrc[i];
+	for (int i = 0; i < n; i++)
+		cDest[i] = tmp[i];
+	free(tmp);
+}
+
+
+int main (int argc, char** argv)
+{
+	char cSrc[] = "Test string"; 
+	char cDest[100]; 
+	memCpy(cDest, cSrc, strlen(cSrc)+1); 
+	printf("Copied string is %s", cDest); 
+  
+	int iSrc[] = {10, 20, 30, 40, 50}; 
+	int n = sizeof(iSrc)/sizeof(iSrc[0]); 
+	int iDest[n], i; 
+	memCpy(iDest, iSrc,  sizeof(iSrc)); 
+	printf("\nCopied array is "); 
+	for (i=0; i<n; i++) 
+		printf("%d ", iDest[i]);
+
+	char mSrc[] = "TestMove";
+	memMove(mSrc+4, mSrc,  strlen(mSrc)+1);
+	//memCpy(mSrc+4, mSrc,  strlen(mSrc)+1);
+	//memmove(mSrc+4, mSrc,  strlen(mSrc)+1);
+	printf("\nMoved array is %s", mSrc);
+	return 0;
+}
+
+
