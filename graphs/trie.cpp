d703828c91cb9cb6d7fc2a64eb04ef543c74737d
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2018-12-17 19:05:54 +0200

    added delete function to the trie

--- graphs/trie.cpp
+++ graphs/trie.cpp
@@ -58,7 +58,53 @@ bool search(struct TrieNode *root, string key)
 	} 
 	return (pCrawl != NULL && pCrawl->isEndOfWord); 
 }
+
+// Returns true if root has no children, else false 
+bool isEmpty(TrieNode* root) 
+{ 
+	for (int i = 0; i < ALPHABET_SIZE; i++) 
+		if (root->children[i]) 
+			return false; 
+	return true; 
+} 
   
+// Recursive function to delete a key from given Trie 
+TrieNode* remove(TrieNode* root, string key, int depth = 0) 
+{ 
+	if (!root) 
+		return NULL; 
+  
+	// If last character of key is being processed 
+	if (depth == key.size()) { 
+  
+        	// This node is no more end of word after  removal of given key 
+		if (root->isEndOfWord) 
+			root->isEndOfWord = false; 
+  
+		// If given is not prefix of any other word 
+		if (isEmpty(root)) { 
+			delete (root); 
+			root = NULL; 
+		} 
+  
+		return root; 
+	} 
+  
+	// If not last character, recur for the child 
+	// obtained using ASCII value 
+	int index = key[depth] - 'a'; 
+	root->children[index] = remove(root->children[index], key, depth + 1); 
+  
+	// If root does not have any child (its only child got  
+	// deleted), and it is not end of another word. 
+	if (isEmpty(root) && root->isEndOfWord == false) { 
+		delete (root); 
+		root = NULL; 
+	} 
+  
+	return root; 
+}
+
 int main() 
 { 
 	// Input keys (only 'a' through 'z' in lower case) 
@@ -74,6 +120,9 @@ int main()
   
 	search(root, "grey")? cout << "Yes\n" : cout << "No\n"; 
 	search(root, "movie")? cout << "Yes\n" : cout << "No\n"; 
+	
+	remove(root, "grey");
+	search(root, "grey")? cout << "Yes\n" : cout << "No\n";
 	return 0; 
 }
 
