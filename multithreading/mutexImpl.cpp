25501535bf90b1a3c51016180988ec5b5d956646
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2018-12-17 22:10:11 +0200

    added mutex impl based on spinlock and semaphore usage sample

---
+++ multithreading/mutexImpl.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+class SpinLock {
+	std::atomic_flag locked;
+public:
+	void lock() {
+		while (locked.test_and_set(std::memory_order_acquire)) { 
+			std::this_thread::yield(); //<- this is not in the source but might improve performance. 
+		}
+	}
+	void unlock() {
+		locked.clear(std::memory_order_release);
+	}
+};
+
+void printSpinLock(SpinLock& mtx1)
+{
+	mtx1.lock();	
+	for (int i = 0; i<5; ++i) {
+		for (int i=0; i<10;++i) {
+			cout << '*';
+			this_thread::sleep_for(chrono::milliseconds(20));
+		}
+		cout << endl;
+	}
+	cout << endl;
+	mtx1.unlock();
+}
+
+
+void printSpinLock2(SpinLock& mtx1)
+{
+	mtx1.lock();
+        for (int i = 0; i<5; ++i) {
+                for (int i=0; i<10;++i) {
+                        cout << '%';
+                        this_thread::sleep_for(chrono::milliseconds(20));                                                                  
+                }
+                cout << endl;
+        }
+        cout << endl;
+	mtx1.unlock();
+}
+
+void print1(mutex& mtx1)
+{
+	mtx1.lock();	
+	for (int i = 0; i<5; ++i) {
+		for (int i=0; i<10;++i) {
+			cout << '*';
+			this_thread::sleep_for(chrono::milliseconds(20));
+		}
+		cout << endl;
+	}
+	cout << endl;
+	mtx1.unlock();
+}
+
+
+void print2(mutex& mtx1)
+{
+	mtx1.lock();
+        for (int i = 0; i<5; ++i) {
+                for (int i=0; i<10;++i) {
+                        cout << '%';
+                        this_thread::sleep_for(chrono::milliseconds(20));                                                                  
+                }
+                cout << endl;
+        }
+        cout << endl;
+	mtx1.unlock();
+}
+
+
+int main()
+{
+	/*mutex mtx;
+	thread t1(print1, ref(mtx));
+	thread t2(print2, ref(mtx));
+	
+	t1.join();
+	t2.join();*/
+
+	SpinLock mtx;
+	thread t1(printSpinLock, ref(mtx));
+	thread t2(printSpinLock2, ref(mtx));
+	
+	t1.join();
+	t2.join();
+
+	return 0;
+}
+
