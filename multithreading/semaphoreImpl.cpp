4b30e855637610110b00fcc4ef7e5124e85ac253
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2018-12-18 19:12:43 +0200

    added samples with semaphores

---
+++ multithreading/semaphoreImpl.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+class Semaphore {
+public:
+	Semaphore (int count_ = 0) : count(count_) 
+	{}
+
+	void notify()
+	{
+		std::unique_lock<std::mutex> lock(mtx);
+		count++;
+		cv.notify_one();
+	}
+
+	void wait()
+	{
+		std::unique_lock<std::mutex> lock(mtx);
+
+		while(count == 0){
+			cv.wait(lock);
+		}	
+		count--;
+	}
+
+private:
+	std::mutex mtx;
+	std::condition_variable cv;
+	int count;
+};
+
+
+void print1(Semaphore& mtx1)
+{
+        mtx1.wait();
+        for (int i = 0; i<5; ++i) {
+                for (int i=0; i<10;++i) {
+                        cout << '*';
+                        this_thread::sleep_for(chrono::milliseconds(20));
+                }
+                cout << endl;
+        }
+        cout << endl;
+        mtx1.notify();
+}
+
+
+void print2(Semaphore& mtx1)
+{
+        mtx1.wait();
+        for (int i = 0; i<5; ++i) {
+                for (int i=0; i<10;++i) {
+                        cout << '%';
+                        this_thread::sleep_for(chrono::milliseconds(20));
+                }
+                cout << endl;
+        }
+        cout << endl;
+        mtx1.notify();
+}
+
+void sleepAndPrint(Semaphore& mtx1, int time)
+{
+        mtx1.wait();
+        this_thread::sleep_for(chrono::milliseconds(time));
+	cout << "finish sleeping"<< endl; 
+        mtx1.notify();
+}
+
+int main()
+{
+	Semaphore sem(2);
+        thread t1(print1, ref(sem));
+        thread t2(sleepAndPrint, ref(sem), 1500);
+
+	// thread 3 will start runing print2 just when print1 or emptyCall will finish the job.
+	thread t3(print2, ref(sem));
+        t1.join();
+        t2.join();
+	t3.join();
+
+	return 0;
+}
