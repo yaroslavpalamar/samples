50757c0f0d0cea3241e323be6c2ce34dcb86a095
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2019-01-30 21:17:33 +0200

    added some implementation to longest increasing subsequence

--- interview_tasks/longestIncreasingSubsequance.cpp
+++ interview_tasks/longestIncreasingSubsequance.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 /*
@@ -11,25 +12,70 @@ using namespace std;
 	6. If there are multiple solutions to the problem, which one should be returned?
 	7. What should I do/return if the input is invalid / does not match the constraints?
 
+	As example: L(i) be the length of the LIS ending at index i such that arr[i] is the last element of the LIS
+	Then, L(i) can be recursively written as:
+	L(i) = 1 + max( L(j) ) where 0 < j < i and arr[j] < arr[i]; or
+	L(i) = 1, if no such j exists.
+
 	Pseudocode:
-		recurs
 		input[200,2,7,77,11,14,4,0,19];
-		vector[0]=input[0];  // 200
-		savedVals[];
-		if (input[i] < [i-1])
-			vector.push_back(input[i]);
-		else
-			longest_increasing_subsequence();
+		if input size 0 return empty vector
+		if input size 1 return first element 
+		create array lis[seqSize] with size of input vector
+		for (int i =0; i< seqSize;i++) each element in vector	
+			set lis[i] to 1
+			for ( int j=0; j < i;j++)
+				if (input[i] > input[j] && lis[i] < lis[j]+1)
+					lis[i]=lis[j]+1
+
+			int lastGoodVal=arr[0];
+			for (int i = 0 ; i < seqSize; i++)
+				if (lastGoodVal+1==lis[i])
+					result.push_back(sequence.at(i))
+					lastGoodVal=arr[i];
+					
 		...
 */
 
 vector<int> longest_increasing_subsequence(vector<int> sequence) {
-    vector<int> result = {0};
-    return result;
+	vector<int> result;
+	int seqSize = sequence.size();
+	if (seqSize == 0)
+		return result;
+	if (seqSize == 1) {
+		result.push_back(sequence.at(0));
+		return result;
+	}
+	int lis[seqSize]; 
+	lis[0] = 1;
+	for (int i = 0; i < seqSize; i++) {
+		lis[i]=1;
+		for (int j = 0; j < i; j++) {
+			if (sequence[i] > sequence[j] && lis[i] < lis[j]+1) {
+				lis[i]=lis[j]+1;
+			}
+		}
+	}
+	for (int i = 0; i < seqSize; i++ ) {
+		vector<int> v;
+		v.push_back(sequence[i]);
+		while (lis[i]==lis[i+1]) {
+			i++;
+			v.push_back(sequence[i]);
+		}
+		result.push_back(*min_element(v.begin(), v.end()));
+	}
+	return result;
 }
 
 int main() {
-	vector<int> test = {200,2,7,77,11,14,4,0,19};
-	longest_increasing_subsequence(test);
+	vector<int> test = {200,199,198,197,2,7,77,11,14,4,0,19};
+	//vector<int> test;
+	//vector<int> test{2};
+	vector<int> result = longest_increasing_subsequence(test);
+	int i=0;
+	for (int val:result) {
+		cout << result[i++] << endl;
+	}
 	return 0;
 }
