06373b8ca3cda8fbc4fbc4893cf2914b9aa2774f
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2019-02-10 22:45:14 +0200

    added comments what diff with std::atomic and atomic_flag and for deadlock

--- multithreading/atomicImpl.cpp
+++ multithreading/atomicImpl.cpp
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+/*
+	1. exist different atomic types: char/short/bool... atomic<bool> ready(false);
+	2. atomic_flag 100% works withou blocking, but std::atomic may be implemented with blocking
+	3. std::this_thread::yield() wait for ready signal
+	4. winner.test_and_set() check if flag is set
+	5. create 10 threads and push them to vector<std::thread>
+	6. every thread will start function countToMillion (int id) {while(!ready){this_thread::yeld();}
+		for (volatile int i=0; i<1000000;++i)
+		if (!winner.test_and_set()){print winner}}
+*/
 atomic<bool> ready(false);
 atomic_flag winner = ATOMIC_FLAG_INIT;
 
--- multithreading/deadlock.cpp
+++ multithreading/deadlock.cpp
@@ -5,6 +5,14 @@
 
 using namespace std;
 
+/*
+	1. recurively lock mutex and you will get dedlock()
+	2. lock/unlock in next sequence to get deadlock:
+		thread1: mtx2.lock(); (((sleep()))) mtx1.lock(); -> mtx1.unlock() mtx2.unlock();
+		thread2: mtx1.lock(); ((((sleep()))) mtx2.lock() -> mtx2.unlock() mtx1.unlock();
+	3. because of sleep one thread wait for mtx2 another wait for mtx1 and it is deadlock.
+*/
+
 mutex mtx1;
 mutex mtx2;
 
