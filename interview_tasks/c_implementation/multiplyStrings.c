9a583a808cb254a44c5b9f843e9415a7fd1ef3f7
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2019-06-06 17:26:49 +0300

    added multiply strings sample

--- interview_tasks/c_implementation/multiplyStrings.c
+++ interview_tasks/c_implementation/multiplyStrings.c
@@ -1,15 +1,60 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define ALLOWED_ARGS_NUM 3
+
 /*
 Input: num1 = "123", num2 = "456"
 Output: "56088"
 */
 
 
+char* multiply(const char* num1, const char* num2){
+	if(*num1=='0' || *num2=='0') 
+		return "0";
+	int len1 = strlen(num1);
+	int len2 = strlen(num2);
+	int len = len1+len2;
+	char *arr = (char*)calloc(sizeof(char)*(len+1), sizeof(char));
+
+	// multiply and save result in arr but not in ascii format.
+	for(int i=len1-1; i > -1; i--) {
+		for(int j=len2-1; j > -1; j--) {
+			arr[i+j+1] += (num1[i]-'0')*(num2[j]-'0');
+			//printf("%d\n",arr[i+j+1]);
+		}
+		for(int i=len-1; i > 0; i--) {
+			arr[i-1] += arr[i]/10;
+			arr[i] %= 10;
+			//printf("%d\n",arr[i]);
+		}
+	}
+
+	
+	// ignore first byte if it 0.
+	int i = 0;
+	if(arr[i]==0) 
+		i++;
+
+	// convert result values to ascii 
+	// also start copy from index i to the begining of arr
+	int resIndex = 0;
+	while(i < len)
+		arr[resIndex++] = arr[i++]+'0';
+	arr[resIndex]='\0';
+
+	return arr;
+}
+
 int
 main(int argc, char**argv)
 {
-	for (int i=0; i<argc; i++) {
-		printf("%s\n", argv[i]);
+	if (argc!=ALLOWED_ARGS_NUM) { 
+		printf ("Please enter two positive integer values for multiplication.\nApp terminated. \n");
+		return 1;
 	}
+	char* result = multiply(argv[1], argv[2]);
+	printf("%s\n", result);
+	free(result);
 	return 0;
 }
