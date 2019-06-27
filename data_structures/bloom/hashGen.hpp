8a854c33f810f1a43733d59d2e4ad29526951a93
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2019-06-27 22:27:53 +0300

    removed unused call of hash function added formatting

--- data_structures/bloom/hashGen.hpp
+++ data_structures/bloom/hashGen.hpp
@@ -8,60 +8,55 @@
 template <typename T, int N>
 class h3
 {
-  static size_t const bits_per_byte =
-    std::numeric_limits<unsigned char>::digits;
+	static size_t const bits_per_byte =
+		std::numeric_limits<unsigned char>::digits;
 
 public:
-  constexpr static size_t byte_range =
-    std::numeric_limits<unsigned char>::max() + 1;
+	constexpr static size_t byte_range =
+		std::numeric_limits<unsigned char>::max() + 1;
 
-  h3(T seed = 0)
-  {
-    T bits[N * bits_per_byte];
-    std::minstd_rand0 prng(seed);
-    for (size_t bit = 0; bit < N * bits_per_byte; ++bit)
-    {
-      bits[bit] = 0;
-      for (size_t i = 0; i < sizeof(T)/2; i++)
-        bits[bit] = (bits[bit] << 16) | (prng() & 0xFFFF);
-    }
+	h3(T seed = 0) {
+		T bits[N * bits_per_byte];
+		std::minstd_rand0 prng(seed);
+		for (size_t bit = 0; bit < N * bits_per_byte; ++bit) {
+			bits[bit] = 0;
+			for (size_t i = 0; i < sizeof(T)/2; i++)
+				bits[bit] = (bits[bit] << 16) | (prng() & 0xFFFF);
+		}
 
-    for (size_t byte = 0; byte < N; ++byte)
-      for (size_t val = 0; val < byte_range; ++val)
-      {
-        bytes_[byte][val] = 0;
-        for (size_t bit = 0; bit < bits_per_byte; ++bit)
-          if (val & (1 << bit))
-            bytes_[byte][val] ^= bits[byte * bits_per_byte + bit];
-      }
-  }
+		for (size_t byte = 0; byte < N; ++byte)
+			for (size_t val = 0; val < byte_range; ++val) {
+				bytes_[byte][val] = 0;
+				for (size_t bit = 0; bit < bits_per_byte; ++bit)
+					if (val & (1 << bit))
+						bytes_[byte][val] ^= bits[byte * bits_per_byte + bit];
+			}
+  	}
 
-  T operator()(void const* data, size_t size, size_t offset = 0) const
-  {
-    auto *p = static_cast<unsigned char const*>(data);
-    T result = 0;
-    // Duff's Device.
-    auto n = (size + 7) / 8;
+	T operator()(void const* data, size_t size, size_t offset = 0) const {
+		auto *p = static_cast<unsigned char const*>(data);
+		T result = 0;
+		// Duff's Device.
+		auto n = (size + 7) / 8;
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
-    switch (size % 8)
-    {
-      case 0:   do { result ^= bytes_[offset++][*p++];
-      case 7:      result ^= bytes_[offset++][*p++];
-      case 6:      result ^= bytes_[offset++][*p++];
-      case 5:      result ^= bytes_[offset++][*p++];
-      case 4:      result ^= bytes_[offset++][*p++];
-      case 3:      result ^= bytes_[offset++][*p++];
-      case 2:      result ^= bytes_[offset++][*p++];
-      case 1:      result ^= bytes_[offset++][*p++];
-              } while ( --n > 0 );
-    }
+		switch (size % 8) {
+			case 0:   do { result ^= bytes_[offset++][*p++];
+			case 7:      result ^= bytes_[offset++][*p++];
+			case 6:      result ^= bytes_[offset++][*p++];
+			case 5:      result ^= bytes_[offset++][*p++];
+			case 4:      result ^= bytes_[offset++][*p++];
+			case 3:      result ^= bytes_[offset++][*p++];
+			case 2:      result ^= bytes_[offset++][*p++];
+			case 1:      result ^= bytes_[offset++][*p++];
+			} while ( --n > 0 );
+		}
 #pragma GCC diagnostic pop
-    return result;
-  }
+	return result;
+	}
 
 private:
-  T bytes_[N][byte_range];
+	T bytes_[N][byte_range];
 };
 
 
@@ -71,16 +66,15 @@ private:
 ///	gi(x) = h1(x) + ih2(x)
 ///Article - http://citeseer.ist.psu.edu/viewdoc/download;jsessionid=4060353E67A356EF9528D2C57C064F5A?doi=10.1.1.152.579&rep=rep1&type=pdf 
 class HashGenerator {
-	constexpr static size_t max_obj_size = 36;
-	h3<size_t, max_obj_size> h3_1;
-	h3<size_t, max_obj_size> h3_2;
-	
-	uint64_t hashA;
-	uint64_t hashB;
+	uint32_t hashA;
+	uint32_t hashB;
 public:
 	HashGenerator(const uint8_t *data, std::size_t len) {
-		hashA = h3_1(data, len);
-		hashB = h3_2(data, len);
+		constexpr static size_t max_obj_size = 32;
+		h3<size_t, max_obj_size> h3_1;
+		uint64_t hash = h3_1(data, len);
+		hashA = (uint32_t)((hash & 0xFFFFFFFF00000000LL) >> 32);
+		hashB = (uint32_t)(hash & 0xFFFFFFFFLL);
 	}
 
 	uint64_t nthHash(uint8_t n, uint64_t filterSize) {
