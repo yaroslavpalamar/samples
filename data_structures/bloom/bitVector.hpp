b676ee1bfd0c2fe4f67dee89443e1428d60de922
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2019-06-27 15:42:53 +0300

    added bit vector implementation

--- data_structures/bloom/bitVector.hpp
+++ data_structures/bloom/bitVector.hpp
@@ -1,4 +1,79 @@
 #ifndef BIT_VECTOR
 #define BIT_VECTOR
 
+#include <cmath>
+
+template <class data_type>
+class BitVector {
+	size_t vec_size;
+	data_type* array;
+	const int BITS_PER_BYTE = 8;
+
+	BitVector(const BitVector& other) = delete;
+	BitVector& operator = (const BitVector& other) = delete;
+	BitVector(const BitVector&& other) = delete;
+	BitVector& operator = (const BitVector&& other) = delete;
+public:
+	explicit BitVector () {
+		this->vec_size = 1;
+		this->array = new data_type[this->vec_size];
+		this->array[0] = 0;
+	}
+	
+	~BitVector () {
+		delete [] this->array;
+	}
+
+	/// Change the capacity of new vector
+	/// @param  {const size_t&} size The new capacity.
+	/// @return {void}
+	void resize(const size_t& size) {
+		unsigned int i;
+		data_type* new_internal_array = new data_type[size];
+		for (i = 0; i < this->vec_size; ++i) {
+			this->array[i] = new_internal_array[i];
+		}
+		this->vec_size = size;
+		delete [] this->array;
+		this->array = new_internal_array;
+	}
+	
+	/// Provides the value of bit at the given position.
+	/// @param  {const size_t&} pos The position within the vector.
+	/// @return {bool}              The success of the look up.
+	bool get_bit(const size_t& pos) const {
+		if (pos < 0 || pos >= this->vec_size * sizeof(data_type) * BITS_PER_BYTE) {
+			throw std::range_error("Attempted to access an illegal position in BitVector.");
+		}
+		int index = floor((pos + 1) / (sizeof(data_type) * BITS_PER_BYTE));
+		int seqCpy = this->array[index];
+		return (seqCpy >> ((sizeof(data_type) * BITS_PER_BYTE) - pos)) & 1;
+	}
+
+	/// Attempts to set a particular bit within the BitVector.
+	/// @param  {const size_t&}  pos  The position within the vector.
+	/// @param  {const bool&} data The bit to set.
+	/// @return {void}
+	void set_bit(const size_t& pos, const bool& data) {
+		if (pos < 0 || pos >= this->vec_size * sizeof(data_type) * BITS_PER_BYTE) {
+			throw std::range_error("Attempted to access an illegal position in BitVector.");
+		}
+		int index = floor((pos + 1) / ((sizeof(data_type) * BITS_PER_BYTE)));
+		if (data) {
+			this->array[index] |= 1 << ((sizeof(data_type) * BITS_PER_BYTE) - pos);
+		} else {
+			this->array[index] &= ~(1 << ((sizeof(data_type) * BITS_PER_BYTE) - pos));
+		}
+	}
+
+	data_type operator[] (const size_t& pos) const {
+		return this->get_bit(pos);
+	}
+
+	size_t size () const {
+		return this->vec_size * sizeof(data_type) * BITS_PER_BYTE;	
+	}
+	
+};
+
 #endif
--- data_structures/bloom/bloomFilterTask.hpp
+++ data_structures/bloom/bloomFilterTask.hpp
@@ -9,49 +9,69 @@
 
 /// Bloom filter sample with insert and check operations
 class BloomFilter {
-	std::vector<bool> bitVector;
+	BitVector<int> bitVector;
 	uint8_t k;
 
-	void insert(const uint8_t* data, const std::size_t& len) 
-	{
+	void insert(const uint8_t* data, const std::size_t& len) {
 		HashGenerator hash(data, len);
         	for (int n = 0; n < k; n++) {
-                	bitVector[hash.nthHash(n, bitVector.size())] = true;
+			try {
+                		bitVector.set_bit(hash.nthHash(n, bitVector.size()), true);
+			} catch (const std::exception& e) {
+				std::cout << e.what() << "\n";
+				std::abort();
+			}
         	}
 	}
 
-	bool check(const uint8_t *data, const std::size_t& len) const 
-	{
+	bool check(const uint8_t *data, const std::size_t& len) const {
 		HashGenerator hash(data, len);
 		for (int n = 0; n < k; n++) {
-			if (!bitVector[hash.nthHash(n, bitVector.size())]) {
-				return false;
-                	}
+			try {
+				if (!bitVector[hash.nthHash(n, bitVector.size())]) {
+					return false;
+             			}	
+			} catch (const std::exception& e) {
+				std::cout << e.what() << "\n";
+				std::abort();
+			}
         	}
 	        return true;
 	}
+	BloomFilter(const BloomFilter& other) = delete;
+	BloomFilter& operator = (const BloomFilter& other) = delete;
+
+	BloomFilter(const BloomFilter&& other) = delete;
+	BloomFilter&& operator = (const BloomFilter&& other) = delete;
+
 public:
 	/// Construct a bit vector and set size of hash functions
 	/// @param m The number of cells
 	/// @param k The number of hash functions
-	BloomFilter(uint64_t m, uint8_t k) : bitVector(m, false)
-	{
+	explicit BloomFilter(uint64_t m, uint8_t k) {
+		bitVector.resize(m);
+		std::cout << "Size on creation: " << bitVector.size() << "\n";
+		try {
+			bitVector.set_bit(m, true);
+		} catch (const std::exception& e) {
+			std::cout << e.what() << "\n";
+			std::abort();
+		}
+
 		this->k = k;
 	}
 	
 	
 	/// Adds an element to the Bloom filter
 	/// @param key The element to insert 
-	void insert(const std::string& key) 
-	{
+	void insert(const std::string& key)  {
 		insert(reinterpret_cast<const uint8_t*>(key.data()),key.size());
 	}
 	
 
 	/// Check if element exist in Bloom filter
 	/// @param key The element to check
-	bool check(const std::string& key) const
-	{
+	bool check(const std::string& key) const {
 		return check(reinterpret_cast<const uint8_t*>(key.c_str()),key.size());
 	}
 };
