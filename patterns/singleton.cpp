9c4395792969b507e4cbbf24c6d3d020ddbe3f22
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2019-07-26 18:40:30 +0300

    added Singleton2 with template

--- patterns/singleton.cpp
+++ patterns/singleton.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 
+template<class T>
+class SingletonT 
+{
+protected:
+	SingletonT() = default;
+	SingletonT(const SingletonT& other) = delete;
+	SingletonT& operator = (const SingletonT& other) = delete;
+
+	SingletonT(const SingletonT&& other) = delete;
+	SingletonT& operator = (const SingletonT&& other) = delete;
+
+public:
+	static T& getInstance() 
+	{
+		static T inst;
+		return inst;
+	}
+
+};
+
+class Singleton2 : public SingletonT<Singleton2> {
+private:
+	Singleton2() = default;
+	Singleton2(const Singleton2& other) = delete;
+	Singleton2& operator = (const Singleton2& other) = delete;
+
+	Singleton2(const Singleton2&& other) = delete;
+	Singleton2& operator = (const Singleton2&& other) = delete;
+
+	friend class SingletonT<Singleton2>;
+};
+
 class Singleton {
 private:
 	Singleton() {
@@ -12,7 +44,7 @@ private:
 	Singleton& operator = (const Singleton& other) = delete;
 
 	Singleton(const Singleton&& other) = delete;
-	Singleton&& operator = (const Singleton&& other) = delete;
+	Singleton& operator = (const Singleton&& other) = delete;
 
 public:
 	static Singleton& getInstance() 
@@ -26,5 +58,6 @@ int
 main () 
 {
 	Singleton& s = Singleton::getInstance();
+	auto &s2 = Singleton2::getInstance();
 	return 0;
 }
