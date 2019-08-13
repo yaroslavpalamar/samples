b7c4ef9a15cdd95351307e3ed3ba0917ad1735cf
Yaroslav Palamar <yaroslavpalamar@gmail.com>
2019-08-13 18:29:19 +0300

    added some comments and impl for binomial coefficient

--- interview_tasks/dynamic_prog_approcah/binomialCoefficient.cpp
+++ interview_tasks/dynamic_prog_approcah/binomialCoefficient.cpp
@@ -6,6 +6,12 @@ using namespace std;
  * and returns the value of Binomial Coefficient C(n, k). 
  * */
 
+/*
+For binomial coefficient exist two formulas and in current implementation we use formual 1:
+1. bC(n-1, k-1) + bC(n-1, k)
+2. n!/k!*(n-k)!
+*/
+
 int binomCoeffRecur(int n, int k)
 {
 	if (k==0 || k==n)
@@ -48,6 +54,22 @@ int binomCoeffDPOptimized(int n, int k)
 	return C[k];
 }
 
+int binomialCoeffFormula2(int n, int k) 
+{  
+	int res = 1;  
+  
+	// Since C(n, k) = C(n, n-k)  
+	if (k > n-k)  
+		k = n - k;  
+  
+	// Calculate value of [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1]  
+	for (int i = 0; i < k; ++i) {  
+		res *= (n - i);  
+		res /= (i + 1);  
+	}
+  
+	return res;  
+}
 
 int
 main(int argc, char** argv)
@@ -57,6 +79,7 @@ main(int argc, char** argv)
 	cout << "Recurs solution: val of C(" << n << "," << k << ") is " << binomCoeffRecur(n, k) << endl;
 	cout << "DP solution: val of C(" << n << "," << k << ") is " << binomCoeffDP(n, k) << endl;
 	cout << "DP optimized solution: val of C(" << n << "," << k << ") is " << binomCoeffDPOptimized(n, k) << endl;
+	cout << "Formula2 n!/k!*(n-k)! = (" << n << "," << k << ") is " << binomialCoeffFormula2(n, k) << endl;
 	return 0;
 }
 
